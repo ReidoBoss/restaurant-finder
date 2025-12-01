11defcc07e4e92609a7641202596e5c79622b344
ReidoBoss <bryansagarino222@gmail.com>
2025-12-01 09:16:35 +0000

    chore: add gemini key env

--- start/env.ts
+++ start/env.ts
@@ -18,4 +18,5 @@ export default await Env.create(new URL('../', import.meta.url), {
   HOST: Env.schema.string({ format: 'host' }),
   LOG_LEVEL: Env.schema.enum(['fatal', 'error', 'warn', 'info', 'debug', 'trace', 'silent']),
   API_SECRET: Env.schema.string(),
+  GEMINI_API_KEY: Env.schema.string(),
 })
